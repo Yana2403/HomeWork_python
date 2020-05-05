56ca24c586122eeecddfed2ec9e47776087b1d6b
Yana2403 <ulendeeva.yana2013@yandex.ru>
2020-05-05 17:01:05 +0400

    Решение задачи 3

--- HomeWork_array/ex3.py
+++ HomeWork_array/ex3.py
@@ -7,16 +7,32 @@ class ex3(object):
         N=int(input(print("Введите число N:")))
         A =[]
         c=N
-        N+=2
-        for i in range (N):
-             for j in range (N):
-              A.append(0)    
-       # A= [[0 for j in range(N)] for i in range(N)] - с этим работает вроде
+        N+=2   
+        A= [[0 for j in range(N)] for i in range(N)] #формирования массива с рамкой из 0
                    
              
-        for i in range(1,c+1,1):
+        for i in range(1,c+1,1): #заполнение массива NxN(входные данные)
             for j in range(1,c+1,1):
-                 A[i][j]=randint(0,25) #'int' object does not support item assignment
+                 A[i][j]=randint(0,5) 
+        print("Исходный массив:")
+        for i in range(1,c+1,1): #вывод начального массива
+            for j in range(1,c+1,1):
+                 print(A[i][j], end=' ')
+            print()
+        Z=[[],[]]
+        Z= [[0 for j in range(c)] for i in range(c)] #Формирование нового массива
+        n=0 #сумма чисел 1 окружения
+        for i in range(1,c+1,1): #сложение чисел 1 окружения
+            for j in range(1,c+1,1):
+                n=A[i][j-1]+A[i-1][j]+A[i+1][j]+A[i][j+1]
+                Z[i-1][j-1]=n
+        print("Результат:")
+        for i in range(len(Z)): #вывод получившегося массива
+            for j in range(len(Z[i])):
+                print(Z[i][j], end=' ')
+            print()
+
+
                                   
      
            
