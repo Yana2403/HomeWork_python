935e79d9e91571efeb88dcb477672938a96b6500
Yana2403 <ulendeeva.yana2013@yandex.ru>
2020-05-05 10:11:58 +0400

    ошибка при работе задачи 3

--- HomeWork_array/ex3.py
+++ HomeWork_array/ex3.py
@@ -2,6 +2,28 @@ class ex3(object):
     @classmethod
     #заменяет в исходном массиве A каждый элемент суммой его 1-окружения (11.2)
     def get_ex_method(self):
-        print("hello")
+        import random
+        from random import randint
+        N=int(input(print("Введите число N:")))
+        A =[]
+        c=N
+        N+=2
+        for i in range (N):
+             for j in range (N):
+              A.append(0)    
+       # A= [[0 for j in range(N)] for i in range(N)] - с этим работает вроде
+                   
+             
+        for i in range(1,c+1,1):
+            for j in range(1,c+1,1):
+                 A[i][j]=randint(0,25) #'int' object does not support item assignment
+                                  
+     
+           
+
+
+
+             
+                 
 
 
